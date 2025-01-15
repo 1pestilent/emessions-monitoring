2672bdeee324147ea2e1ff8e56c9bc4016f204dd
Mikhail <1pestilent@gmail.com>
2025-01-15 12:21:13 +0500

    первоначальный вид панель - готов

--- app/front-end/scripts/index.js
+++ app/front-end/scripts/index.js
@@ -17,3 +17,26 @@ options: {
     }
 }
 });
+
+window.addEventListener("load", () => {
+    window.addEventListener("mouseup", (e) => {
+        if (!e.target.closest(".js-select")) {
+            const allSelectors = document.querySelectorAll(".js-select-dropdown");
+            allSelectors.forEach(select => select.classList.remove("expanded"))
+            return;
+        } else {
+            const container = e.target.closest(".js-select")
+            const dropdown = container.querySelector(".js-select-dropdown")
+            dropdown.classList.toggle("expanded");
+        }
+    });
+    
+    window.addEventListener("mouseup", (e) => {
+        if (!e.target.closest(".js-select-item")) return;
+        const container = e.target.closest(".js-select");
+        const item = e.target.closest(".js-select-item");
+        const input = container.querySelector(".js-select-input");
+        const value = item.dataset.value;
+        input.value = value;
+    })
+});
