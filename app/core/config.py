667f9430c363e448b46c5c34f8ae8cbb8eddcf89
Mikhail <1pestilent@gmail.com>
2025-02-13 18:39:43 +0500

    добавил хеширование паролей и создание jwt

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,26 @@
+from pathlib import Path
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Параметры базы данных
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
+
+# Авторизация
+PRIVATE_KEY_PATH: Path = BASE_DIR / "certs" / "jwt-private.pem"
+PUBLIC_KEY_PATH: Path = BASE_DIR / "certs" / "jwt-public.pem"
+ALGORITHM: str = "RS256"
+
+TOKEN_TYPE_FIELD = "type"
+ACCESS_TOKEN_TYPE = "access"
+REFRESH_TOKEN_TYPE = "refresh"
+
+ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+REFRESH_TOKEN_EXPIRE_DAYS: int = 14
---
+++ app/core/security.py
@@ -0,0 +1,48 @@
+import bcrypt
+import jwt
+from datetime import datetime, timedelta
+
+from app.core.config import PUBLIC_KEY_PATH, PRIVATE_KEY_PATH, TOKEN_TYPE_FIELD, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
+def hash_password(password: str) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def verify_password(password: str, hashed_password: bytes) -> bool:
+    return bcrypt.checkpw(password=password.encode(), hashed_password=hashed_password)
+
+def encode_jwt(
+        token_type: str,
+        payload: dict,
+        expire_minutes: int = ACCESS_TOKEN_EXPIRE_MINUTES,
+        expire_timedelta: timedelta | None = None,
+        ) -> str:
+
+    now = datetime.utcnow()
+    to_encode = payload.copy()
+
+
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+
+    to_encode.update(
+        TOKEN_TYPE_FIELD = token_type,
+        iat = now,
+        exp = expire
+        )
+
+    encoded = jwt.encode(to_encode, PRIVATE_KEY_PATH.read_text(), ALGORITHM)
+    return encoded
+
+def decode_jwt(token: str | bytes) -> dict:
+
+    decoded = jwt.decode(
+        token,
+        PUBLIC_KEY_PATH.read_text(),
+        algorithms=[ALGORITHM]
+        )
+    return decoded
