476d2b534ed65c6dbf10d71156ae11c58d357060
Mikhail <1pestilent@gmail.com>
2025-03-03 12:44:04 +0500

    переработал структуру бд

--- app/models/substances.py
+++ app/models/substances.py
@@ -1,20 +1,51 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String, Text, Float, DateTime, text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.database import Base
+
+class StatusModel(Base):
+    __tablename__ = 'status'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(31), nullable=False, unique=True)
 
 class UnitModel(Base):
     __tablename__ = 'units'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
-    symbol: Mapped[str]
-    
+    name: Mapped[str] = mapped_column(String(63), nullable=False, unique=True)
+    symbol: Mapped[str] = mapped_column(String(15), nullable=False, unique=True)
 
-class SubstanceModel(Base):
-    __tablename__ = "substances"
+class SensorModel(Base):
+    __tablename__ = 'sensors'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
-    unit_id: Mapped[int] = mapped_column(ForeignKey('units.id',ondelete='RESTRICT'))
-    mpc: Mapped[float]
+    name: Mapped[str] = mapped_column(String(127), nullable=False)
+    serial_number: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    unit_id: Mapped[int] = mapped_column(ForeignKey('units.id', ondelete='RESTRICT'))
+    installation_date: Mapped[DateTime]
+    calibration_date:  Mapped[DateTime]
+    description: Mapped[Text]
+    status: Mapped[int] = mapped_column(ForeignKey('status.id', ondelete='RESTRICT'))
+
+class SensorReadingsModel(Base):
+    __tablename__ = 'readings'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    sensor_id: Mapped[int] = mapped_column(ForeignKey('sensors.id', ondelete='RESTRICT'))
+    value: Mapped[float] = mapped_column(Float)
+    timestamp: Mapped[DateTime]
+
+class LocationModel(Base):
+    __tablename__ = 'locations'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(127), nullable=False, unique=True)
+    description: Mapped[Text]
+
+class SensorMappingModel(Base):
+    __tablename__ = 'sensors_mapping'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    sensor_id: Mapped[int] = mapped_column(ForeignKey('sensors.id', ondelete='RESTRICT'))
+    location_id: Mapped[int] = mapped_column(ForeignKey('locations.id', ondelete='RESTRICT'))
