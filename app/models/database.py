37f7738370f4c46a8923deddbdb136e7a4eaf936
Mikhail <1pestilent@gmail.com>
2025-03-17 14:30:47 +0500

    добавил возможность использовать редис для кеширования данных

--- app/models/database.py
+++ app/models/database.py
@@ -1,14 +1,14 @@
-import pickle
+
 from typing import Annotated
 
 from fastapi import Depends
-from redis import Redis
+
 from sqlalchemy.ext.asyncio import (AsyncAttrs, AsyncSession,
                                     create_async_engine)
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-from app.core.config import (DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER,
-                             REDIS_PORT)
+from app.core.config import (DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER)
+
 
 engine = create_async_engine(f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
@@ -26,8 +26,6 @@ class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 async def setup_database():
-    from app.models import users
-
     async with engine.begin() as con:
         await con.run_sync(Base.metadata.drop_all)
         await con.run_sync(Base.metadata.create_all)
@@ -36,7 +34,3 @@ async def setup_database():
 
 
 session_dependency = Annotated[AsyncSession, Depends(get_session)]
-
-def connect_to_redis():
-    return Redis(host=DB_HOST, port=REDIS_PORT)
-
